c00b878739ad8679a84ffdd7a7ed174afa199834
Andrey Klimkov <aaklimkov@gmail.com>
2024-01-14 20:35:54 +0300

    add sequence of numbers and clear display, add test for add numbers

--- src/Calculator/calculator.js
+++ src/Calculator/calculator.js
@@ -5,4 +5,21 @@ export default class Calculator {
     this.previousOperand = null;
     this.operation = null;
   }
+
+  enterNumber(number) {
+    if (this.displayValue === "0" || this.currentOperand === null) {
+      this.displayValue = number;
+      this.currentOperand = number;
+    } else {
+      this.displayValue += number;
+      this.currentOperand += number;
+    }
+  }
+
+  clear() {
+    this.displayValue = "0";
+    this.currentOperand = null;
+    this.previousOperand = null;
+    this.operation = null;
+  }
 }
--- test/calc.test.js
+++ test/calc.test.js
@@ -15,4 +15,24 @@ describe("Calculator", () => {
     calc.enterNumber("1");
     expect(calc.displayValue).toBe("1");
   });
+
+  test("enters a sequence of numbers", () => {
+    calc.enterNumber("1");
+    calc.enterNumber("2");
+    expect(calc.displayValue).toBe("12");
+  });
+
+  test("clears display", () => {
+    calc.enterNumber("1");
+    calc.clear();
+    expect(calc.displayValue).toBe("0");
+  });
+
+  test("adds two numbers", () => {
+    calc.enterNumber("1");
+    calc.chooseOperation("+");
+    calc.enterNumber("2");
+    calc.compute();
+    expect(calc.displayValue).toBe("3");
+  });
 });
