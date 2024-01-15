05b2cb808ab4ab3638f7d47fe2613b667dd9c725
Andrey Klimkov <aaklimkov@gmail.com>
2024-01-15 14:06:57 +0300

    version for release

--- src/UI/updateDisplay.js
+++
@@ -1,8 +0,0 @@
-export default function updateDisplay(calculator) {
-  const previousOperandTextElement =
-    document.querySelector(".previous-operand");
-  const currentOperandTextElement = document.querySelector(".current-operand");
-
-  previousOperandTextElement.textContent = calculator.previousOperand;
-  currentOperandTextElement.textContent = calculator.currentOperand;
-}
